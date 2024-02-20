649d50037fefd42c57f43219a2c72eb667f55da1
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-02-20 12:43:23 +0000

    Arrays Types

--- project/app.js
+++ project/app.js
@@ -4,6 +4,14 @@
 } = {*/
 var person = {
     name: 'Andrew',
-    age: 43
+    age: 43,
+    hobbies: ['Reading', 'Cooking']
 };
+var favouriteActivities;
+favouriteActivities = ['Reading'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map());  // !!! ERROR !!!
+}
--- project/app.ts
+++ project/app.ts
@@ -4,7 +4,16 @@
 } = {*/
 const person = {
   name: 'Andrew',
-  age: 43
+  age: 43,
+  hobbies: ['Reading', 'Cooking']
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ['Reading'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //console.log(hobby.map());  // !!! ERROR !!!
+}
