7f2ad510a42462088df26df3b765d380bb55cfe3
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-02-21 12:15:59 +0000

    The "never" Type

--- project/app.js
+++ project/app.js
@@ -5,3 +5,10 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    while (true) { }
+}
+//const result = generateError('An error occurred!', 500);
+//console.log(result);
+generateError('An error occurred!', 500);
