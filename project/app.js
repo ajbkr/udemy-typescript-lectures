f686c43778ede172c7417da2b77d439a3ab13738
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-02-21 12:05:48 +0000

    The "unknown" Type

--- project/app.js
+++ project/app.js
@@ -1,22 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-//let combineValues: Function;
-//let combineValues: ((a: number, b: number) => number) | ((a: number) => void);
-var combineValues;
-combineValues = add;
-//combineValues = printResult;
-//combineValues = 5;
-console.log(combineValues(8, 8));
-//let someValue: undefined;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
