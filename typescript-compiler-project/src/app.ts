b46b87ff56debe7ae4e40c6d3dc8e66af7869b66
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-04-11 12:44:02 +0100

    Code Quality Options

--- typescript-compiler-project/src/app.ts
+++ typescript-compiler-project/src/app.ts
@@ -1,7 +1,16 @@
+let appId = 'abc';
 //const button = document.querySelector('button')!;
 const button = document.querySelector('button');
 
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return 0;
+}
+
 function clickHandler(message: string) {
+  // let userName = 'Max';
   console.log('Clicked! ' + message);
 }
 
