a6e75122099bf3a399f0554438c84bc38c79f8bc
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-04-13 11:38:22 +0100

    "private" and "public" Access Modifiers

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,19 +1,37 @@
 class Department {
-  name: string;
+  /* public */ name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
 
+  addEmployee(employee: string) {
+    // validation, etc.
+    this.employees.push(employee);
+  }
+
   describe(this: Department) {
     console.log('Department: ' + this.name);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
+
 accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+/* const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
-accountingCopy.describe();
+accountingCopy.describe(); */
