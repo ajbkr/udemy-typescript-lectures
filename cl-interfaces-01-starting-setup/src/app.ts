c0db6c264f33647885b5ee98927a2e887ff8980a
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-04-13 12:44:50 +0100

    Getters & Setters

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -34,8 +34,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value!');
+    }
+    this.addReport(value);
   }
 
   addEmployee(name: string) {
@@ -47,6 +64,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -57,7 +75,11 @@ class AccountingDepartment extends Department {
 const accounting = new AccountingDepartment('d1', []);
 const it = new ITDepartment('d2', ['Max']);
 
+accounting.mostRecentReport = 'Year End Report';
+
 accounting.addReport('Something went wrong...');
+
+console.log(accounting.mostRecentReport);
 
 accounting.printReports();
 
