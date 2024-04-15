1ee718207533c3b67c37d0eb6fd6e8b68c395ef9
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-04-15 09:59:46 +0100

    DOM Element Selection & OOP Rendering

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    // this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this._attach();
+  }
+
+  private _attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
