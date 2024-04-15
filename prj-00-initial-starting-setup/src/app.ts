12c01f0bb222eea8005a043fa8766b060acc30d2
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2024-04-15 10:12:05 +0100

    Interacting with DOM Elements

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -3,6 +3,10 @@ class ProjectInput {
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
 
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
     // this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,11 +14,28 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this._configure();
     this._attach();
   }
 
   private _attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private _configure() {
+    this.element.addEventListener('submit', this._submitHandler.bind(this));
+  }
+
+  private _submitHandler(event: Event) {
+    event.preventDefault();
+
+    console.log(this.titleInputElement.value);
   }
 }
 
